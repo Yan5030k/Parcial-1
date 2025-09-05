2a7e9b98defd9bc90bc2cd2044f395b0c437a375
Jhoan Ortega <jhoanortega2022@gmail.com>
2025-09-05 07:10:26 -0600

    Creación del repositorio

---
+++ Ejercicio1.py
@@ -0,0 +1,8 @@
+"""
+En El Salvador, los comerciantes de mercados locales desean llevar un
+control organizado de sus ventas diarias. Cada venta incluye tipos variados
+de productos con ciertas características y precios ya definidos. Sin un registro,
+es difícil saber cuáles productos generan más ingresos y cómo planificar el
+inventario de manera eficiente.
+"""
+
